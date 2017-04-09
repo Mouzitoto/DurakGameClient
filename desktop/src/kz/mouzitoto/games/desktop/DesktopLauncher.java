c8fc730cb2465caac16904f71e5cda7e24db2702
Mouzitoto <Mouzitoto@gmail.com>
2017-04-09 10:01:41 +0600

    first commit

---
+++ desktop/src/kz/mouzitoto/games/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package kz.mouzitoto.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import kz.mouzitoto.games.DGClient;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DGClient(), config);
+	}
+}
---
+++ html/src/kz/mouzitoto/games/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package kz.mouzitoto.games.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import kz.mouzitoto.games.DGClient;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new DGClient();
+        }
+}
